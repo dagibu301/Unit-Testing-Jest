3bb00d8227205a80c5b778bca6c37a1f19d865c5
David Giraldo <davidgiraldobueno@gmail.com>
2021-06-16 16:47:07 -0500

    callback

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,20 +1,30 @@
 const { test, expect } = require("@jest/globals");
 
-const text = 'Hello World!';
-const fruits = ['apple', 'orange', 'banana'];
+const text = "Hello World!";
+const fruits = ["apple", "orange", "banana"];
 
-test('It should have a text', () => {
-    expect(text).toMatch(/World/);
+test("It should have a text", () => {
+  expect(text).toMatch(/World/);
 });
 
-test('It should have a banana', () => {
-    expect(fruits).toContain('banana');
+test("It should have a banana", () => {
+  expect(fruits).toContain("banana");
 });
 
-test('It should be greater', () => {
-    expect(10).toBeGreaterThan(9);
+test("It should be greater", () => {
+  expect(10).toBeGreaterThan(9);
 });
 
-test('It should be truthy', () => {
-    expect(true).toBeTruthy();
+test("It should be truthy", () => {
+  expect(true).toBeTruthy();
+});
+
+const reverseString = (str, callback) => {
+  callback(str.split("").reverse().join(""));
+};
+
+test("Test a callback", () => {
+  reverseString("Hello", (str) => {
+    expect(str).toBe("olleH");
+  });
 });
